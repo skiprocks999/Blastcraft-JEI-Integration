2902ed084ccba379ec0402ba29f4bc257e5cf41d
skip999 <skiprocks999@gmail.com>
2025-01-04 10:47:25 -0600

    add fluids to JEI menu

--- src/main/java/blastcraft/compatibility/jei/BlastCraftJEIPlugin.java
+++ src/main/java/blastcraft/compatibility/jei/BlastCraftJEIPlugin.java
@@ -1,23 +1,26 @@
 package blastcraft.compatibility.jei;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 import blastcraft.common.recipe.BlastCraftRecipeInit;
 import blastcraft.common.recipe.categories.item2item.specificmachines.BlastCompressorRecipe;
 import blastcraft.compatibility.jei.recipecategories.item2item.specificmachines.BlastCompressorRecipeCategory;
+import blastcraft.registers.BlastcraftFluids;
 import electrodynamics.client.screen.tile.ScreenO2OProcessor;
 import electrodynamics.compatibility.jei.ElectrodynamicsJEIPlugin;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
-import mezz.jei.api.registration.IGuiHandlerRegistration;
-import mezz.jei.api.registration.IRecipeCatalystRegistration;
-import mezz.jei.api.registration.IRecipeCategoryRegistration;
-import mezz.jei.api.registration.IRecipeRegistration;
+import mezz.jei.api.neoforge.NeoForgeTypes;
+import mezz.jei.api.registration.*;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.RecipeManager;
+import net.minecraft.world.level.material.Fluid;
+import net.neoforged.neoforge.fluids.FluidStack;
+import net.neoforged.neoforge.registries.DeferredHolder;
 
 @JeiPlugin
 public class BlastCraftJEIPlugin implements IModPlugin {
@@ -52,6 +55,15 @@ public class BlastCraftJEIPlugin implements IModPlugin {
     @Override
     public void registerGuiHandlers(IGuiHandlerRegistration registry) {
         registry.addRecipeClickArea(ScreenO2OProcessor.class, 48, 35, 22, 15, ElectrodynamicsJEIPlugin.O2O_CLICK_AREAS.toArray(new mezz.jei.api.recipe.RecipeType[ElectrodynamicsJEIPlugin.O2O_CLICK_AREAS.size()]));
+    }
+
+    @Override
+    public void registerExtraIngredients(IExtraIngredientRegistration registration) {
+        List<FluidStack> fluids = new ArrayList<>();
+        for (DeferredHolder<Fluid, ? extends Fluid> fluid : BlastcraftFluids.FLUIDS.getEntries()) {
+            fluids.add(new FluidStack(fluid.get(), 1000));
+        }
+        registration.addExtraIngredients(NeoForgeTypes.FLUID_STACK, fluids);
     }
 
 }
